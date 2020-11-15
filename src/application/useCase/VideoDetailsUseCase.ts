14f4f4ab555c4089f4f8a71f7f8e8b6a91136d30
Alven Alinan <alven.alinan@accedo.tv>
2020-11-15 23:06:53 +0800

    Updated demo template

--- src/application/useCase/VideoDetailsUseCase.ts
+++ src/application/useCase/VideoDetailsUseCase.ts
@@ -1,11 +1,11 @@
 import UseCase from "../../abstract/UseCase";
 
-// Data Transfer Object - what it should accept from interface
+// Data Transfer Object - what it should accept from User Interface (UI)
 type DetailsRequestDTO = {
   id: string;
 };
 
-// Data Transfer Object - what it should return back to interface
+// Data Transfer Object - what it should return back to User Interface (UI)
 type DetailsResponseDTO = {
   id: string;
   title: string;
@@ -14,11 +14,12 @@ type DetailsResponseDTO = {
   reference: string;
 };
 
+// implements the interface UseCase
+// execute() is the entry point per use case
 export class VideoDetailsUseCase implements UseCase<DetailsRequestDTO, DetailsResponseDTO> {
-    constructor() {
-    }
+  constructor() {}
 
-    async execute(args: DetailsRequestDTO): Promise<DetailsResponseDTO> {
-        throw new Error('Not yet implemented');
-    }
+  async execute(args: DetailsRequestDTO): Promise<DetailsResponseDTO> {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/ui/http/controller.ts
+++ src/ui/http/controller.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import { VideoDetailsUseCase } from '../../application/useCase/VideoDetailsUseCase';
 
 const router = express.Router();
 
 // REST Controller
 router.get('/api/v1/video/:id', async (req, res) => {
-});
+  const { params } = req;
 
+  // extracted the id from the request path params
+  const { id } = params;
+});
 
 export { router };
